595b9e472eab24f72b2f86ae89f52fae866e87a0
Jakub Wasilewski <jakub.wasilewski@schibsted.pl>
2014-08-26 09:37:39 +0200

    New build.sh script that also minifies HTML and CSS.

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-uglifyjs erisk.js -m toplevel -c -o dist/erisk-min.js || exit 1
+# Minify Javascript
+uglifyjs erisk.js -m toplevel -c -o dist/erisk.js || exit 1
+# Minify HTML
+html-minifier --minify-css --collapse-whitespace index.html -o dist/index.html
 
-ZIP_FILE=dist/erisk.zip 
-ZIPPED_FILES="index.html dist/erisk-min.js"
+# Zip the whole thing
+ZIP_FILE=erisk.zip 
+ZIPPED_FILES="index.html erisk.js"
+
+cd dist
+rm -f $ZIP_FILE
 zip -q9 $ZIP_FILE $ZIPPED_FILES || exit 1
+cd ..
 
-FINAL_SIZE=$( wc -c "$ZIP_FILE" | awk '{print $1}')
+# Report
+FINAL_SIZE=$( wc -c "dist/$ZIP_FILE" | awk '{print $1}')
 echo "Final size: $FINAL_SIZE bytes"
