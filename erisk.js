c47fe0827682ef54e5235b539ddd10b3b97e38f6
Jakub Wasilewski <wasyl@wasyl.eu>
2014-09-26 20:21:41 +0200

    Particle effects, if you can call it that.

--- erisk.js
+++ erisk.js
@@ -755,6 +755,7 @@ function updateMapDisplay(gameState) {
             // this is an ex-div - in other words, the soldier it represented is dead
             $('m').removeChild(div);
             delete soldierDivsById[id]; // surprisingly, this should be safe to do during iteration - http://stackoverflow.com/a/19564686
+            
         }
     });
 
@@ -767,6 +768,7 @@ function updateMapDisplay(gameState) {
         var highlighted = contains(gameState.d && gameState.d.h || [], region) ||    // a region is highlighted if it has an available move
                           (gameState.e && regionOwner == gameState.e);               // - or belongs to the winner (end game display highlights the winner)
 
+        // highlighting
         if (highlighted) {
             gradientName += 'h';
         }
@@ -776,6 +778,20 @@ function updateMapDisplay(gameState) {
         region.hl.style.opacity = highlighted ? highlightedOpacity : 0.0;
         region.hl.style.cursor = highlighted ? 'pointer' : 'default';
 
+        // particles
+        if (gameState.prt == region) {
+            gameState.prt = 0; // only once
+            map(region.p, function(point) {
+                point = projectPoint(point);
+                var center = region.c;
+                var alpha = rint(30,100)/100;
+                var startPoint = [lerp(alpha,center[0],point[0]), lerp(alpha,center[1],point[1])];
+                var vx = (startPoint[0]-center[0]) / 2, vy = (startPoint[1]-center[1]) / 2 - 0.15;
+                spawnParticle(startPoint[0], startPoint[1], vx, vy, '#fff');
+            });
+        }
+
+        // fill
         region.e.style.fill = 'url(#' + gradientName + ')';
     }
 
@@ -1013,13 +1029,16 @@ function showBanner(background, text, delay) {
     }
 }
 
-function spawnParticle(x, y, color) {
-    var styles = "opacity:1;left: " + x + "%;top: " + y + "%;box-shadow: 0 0 3px 3px " + color;
-    var particle = append('m', div({c: 'pr', s: styles}, ''));
+function spawnParticle(x, y, vx, vy, color) {
+    var styleString = "opacity:1;left: " + x + "%;top: " + y + "%;box-shadow: 0 0 1px 1px " + color;
+    var particle = append('m', div({c: 'pr', s: styleString}, ''));
     setTimeout(function() {
-        particle.style.top = (y-10) + "%";
+        setTransform(particle, "translate3d(" + vx + "em," + vy + "em,0)");
         particle.style.opacity = 0.0;
     }, 10);
+    setTimeout(function() {
+        $('m').removeChild(particle);
+    }, 1050);
 }
 
 function preserveAspect() {
@@ -1761,7 +1780,8 @@ function moveSoldiers(state, fromRegion, toRegion, incomingSoldiers) {
             var temple = state.t[toRegion.i];
             if (temple)
                 delete temple.u;
-            // play sound!
+            // play sound, launch particles!
+            state.prt = toRegion;
             if (defendingSoldiers)
                 state.sc = audioVictory;
         }
